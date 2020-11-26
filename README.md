733e59ee8cb96a87c443dc18692ca54b9420ecdb
Raphael Ullrich <ulzea@student.kit.edu>
2020-11-26 12:27:10 +0100

    final commit

---
+++ README.md
@@ -0,0 +1,71 @@
+<!-- Headings -->
+# stereo_camera_testbed
+This repository contains software which has been developed for a bachelor thesis at the HERA (Health Robotic and Automation Lab) at KIT.
+The topic of the thesis is: "Development of a Testbed for Visual Based Tracking and Localization of Continuum Robots".
+
+The package has been tested under ROS Noetic on Ubuntu 20.04. This is research code and any fitness for a particular purpose is disclaimed. 
+
+## Prerequisites
+This code is embedded within the ROS framework.
+For this reason, make sure ROS Noetic is installed on Ubuntu 20.04 and your catkin workspace is set up. 
+
+Go to [ROS-Wiki](http://wiki.ros.org/noetic/Installation/Ubuntu) if you need to install ROS Noetic. 
+
+The repository is organized as a ROS package and its functionality depends on topics published other ROS packages. 
+For this reason, make sure topics provided in [ROS-image_pipeline](http://wiki.ros.org/image_pipeline?distro=noetic) packages are available. 
+
+The [stereo_image_proc](http://wiki.ros.org/stereo_image_proc?distro=noetic) within the image_pipeline package is dependent on camera image- and camera information topics as well as calibrated stereo cameras. 
+
+Stereo calibration can be done with the [camera_calibration](http://wiki.ros.org/camera_calibration?distro=noetic) package provided in image_pipeline. 
+ 
+## How to use?
+
+Run stereo camera calibration tool.
+```
+rosrun camera_calibration cameracalibrator.py --approximate 0.1 --size 7x6 --square 0.015 right:=/stereo/right/image_raw left:=/stereo/left/image_raw right_camera:=/stereo/right left_camera:=/stereo/left
+```
+Adjust size and square length according to the used chessboard and its dimensions. 
+
+If camera calibration was successful and calibration data is stored, run the stereo_image_proc node with the following command:
+```
+ROS_NAMESPACE=stereo rosrun stereo_image_proc stereo_image_proc approximate_sync:=true queue_size= 20
+```
+Next, run 
+```
+rviz
+```
+and subscribe to the points2 topic published by the stereo_image_proc node.
+Adjustments to the stereo matching algorithms can be done with the help of rqt configure. For this, run the following command from a new terminal.
+```
+rosrun rqt_reconfigure rqt_reconfigure
+```
+### extrinsicCalibration node
+Three dimensional backbone points can be processed in a predefined frame by doing an extrinsic calibration with the help of a chessboard pattern.
+By running
+```
+rosrun stereo_testbed extrinsicCalibration
+```
+the coordinate transformation between camera frame and chessboard frame origin is computed. 
+
+### skeleton node
+By starting the skeleton node, extrinisic calibration data is used to express all backbone points in the coordinate frame defined by the user.
+```
+rosrun stereo_testbed skeleton 
+```
+(Notice: Best results are achieved by placing the observed continuum robot behind a uni-colored background)
+
+Calculated three dimensional backbone points are published under the SkeletonPoints topic.
+
+### parametricCurveFitting node
+Inside the parametricCurveFitting node, functionality for ordering and approximating backbone points provided by the SkeletonPoints topic is given.
+At the current state, the node is adjusted for single execution and therefore it will use the current backbone points published by the skeleton node. 
+Points are sorted with the help of Dijkstra's algorithm and approximated by a third degree B-spline. Notice that the source for the Dijstra sort is given by the point with the lowest X-coordinate. 
+```
+rosrun stereo_testbed parametricCurveFitting.py 
+```
+
+
+
+
+
+
--- src/extrinsicCalibration.cpp
+++ src/extrinsicCalibration.cpp
@@ -28,112 +28,89 @@ private:
 public:
 
 
-   void findCorners(cv::Mat img,std::vector<cv::Point2f>& cornersFound)
+void findCorners(cv::Mat img,std::vector<cv::Point2f>& cornersFound)
+{
+   corners_found = findChessboardCorners(img, boardSize, cornersFound, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
+   if (corners_found)
    {
+     cv::drawChessboardCorners(img, boardSize, cornersFound, corners_found);
 
-      corners_found = findChessboardCorners(img, boardSize, cornersFound, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
-       if (corners_found)
-       {
-         cv::drawChessboardCorners(img, boardSize, cornersFound, corners_found);
-
-          cv::circle(img, cornersFound[0] , 8, cv::Scalar( 255, 255, 255 ),-2,8,0);
-          imshow("detected corners", img);
-
-       }
-       else
-       {
-         std::cout<<"ERROR couldn't detect corners! Please move Chessboard into field of view"<< std::endl;
-       }
+      cv::circle(img, cornersFound[0] , 8, cv::Scalar( 255, 255, 255 ),-2,8,0);
+      imshow("detected corners", img);
    }
-
-   //populate
-   void realWorld3DChessboardPosition( cv::Size boardSize, float squareEdgeLength_, std::vector<cv::Point3f>& corners_1)
+   else
    {
-     for (int i = 0; i < boardSize.height; i++)
-         {
-             for (int j = 0; j < boardSize.width; j++)
-             {
-                 corners_1.push_back(cv::Point3f(j * squareEdgeLength_, i * squareEdgeLength_, 0.0f));
-
-             }
-
-         }
-
-
-
+     std::cout<<"ERROR couldn't detect corners! Please move Chessboard into field of view"<< std::endl;
    }
+}
+void realWorld3DChessboardPosition( cv::Size boardSize, float squareEdgeLength_, std::vector<cv::Point3f>& corners_1)
+{
+   for (int i = 0; i < boardSize.height; i++)
+     {
+        for (int j = 0; j < boardSize.width; j++)
+        {
+          corners_1.push_back(cv::Point3f(j * squareEdgeLength_, i * squareEdgeLength_, 0.0f));
+        }
+     }
+}
+void imageCallback(const sensor_msgs::ImageConstPtr& image_, const sensor_msgs::CameraInfoConstPtr& camera_info)
+{
+   cv::Mat frame = cv_bridge::toCvShare(image_, "bgr8")->image;
+   std::vector<cv::Point3f> worldSpaceCornerPoints;
+   std::vector<cv::Point2f> chessboardImageSpacePoints;
+   std::vector<std::vector<double>> CameraMatrix = {{0,0,0},{0,0,0},{0,0,0}};
+   cv::Mat_<double> camMatrix(3,3);
+   try
+   {
+     cv::imshow("view", frame);
+     CalibrationMatrix = camera_info->K;
+     DistortionMatrix = camera_info->D;
+     int k = 0;
+     for (int i=0 ; i<3 ;  i++)
+     {
+       for (int c=0 ; c<3 ;  c++)
+       {
+         camMatrix(i,c) = CalibrationMatrix[k];
+         k++;
+       }
+     }
 
+     findCorners(frame,chessboardImageSpacePoints);
+     realWorld3DChessboardPosition(boardSize,squareEdgeLength, worldSpaceCornerPoints);
 
-  void imageCallback(const sensor_msgs::ImageConstPtr& image_, const sensor_msgs::CameraInfoConstPtr& camera_info)
-  {
-    cv::Mat frame = cv_bridge::toCvShare(image_, "bgr8")->image;
-    std::vector<cv::Point3f> worldSpaceCornerPoints;
-    std::vector<cv::Point2f> chessboardImageSpacePoints;
-    std::vector<std::vector<double>> CameraMatrix = {{0,0,0},{0,0,0},{0,0,0}};
-    cv::Mat_<double> camMatrix(3,3);
-    try
-    {
-      cv::imshow("view", frame);
-
-      CalibrationMatrix = camera_info->K;
-      DistortionMatrix = camera_info->D;
-
-      int k = 0;
-      for (int i=0 ; i<3 ;  i++)
-      {
-        for (int c=0 ; c<3 ;  c++){
-
-        camMatrix(i,c) = CalibrationMatrix[k];
-        k++;
-      }
-
-      }
-
-      findCorners(frame,chessboardImageSpacePoints);
-      realWorld3DChessboardPosition(boardSize,squareEdgeLength, worldSpaceCornerPoints);
-
-      if (corners_found)
-      {
-      cv::solvePnP(worldSpaceCornerPoints, chessboardImageSpacePoints,camMatrix,DistortionMatrix, rotation,translation );
-      std::cout<< "rotation: ";
-      CameraTransform_msg.rvec.x=  rotation[0];
+     if (corners_found)
+     {
+       cv::solvePnP(worldSpaceCornerPoints, chessboardImageSpacePoints,camMatrix,DistortionMatrix, rotation,translation );
+       std::cout<< "rotation: ";
+       CameraTransform_msg.rvec.x=  rotation[0];
        CameraTransform_msg.rvec.y=  rotation[1];
-        CameraTransform_msg.rvec.z=  rotation[2];
-                  for (int j=0; j<3 ; j++)
-      {
-
-      std::cout << rotation[j];
-      }
-
+       CameraTransform_msg.rvec.z=  rotation[2];
+       for (int j=0; j<3 ; j++)
+       {
+         std::cout << rotation[j];
+       }
        CameraTransform_msg.tvec.x=  translation[0];
-        CameraTransform_msg.tvec.y=  translation[1];
-         CameraTransform_msg.tvec.z=  translation[2];
-      std::cout<<  "    translation: ";
-                   for (int k=0 ; k<3 ; k++)
-      {
-
-      std::cout << translation[k] ;
-      }
-      }
-
-      cv::waitKey(10);
-      }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("Could not convert from '%s' to 'bgr8'.", image_->encoding.c_str());
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-    }
+       CameraTransform_msg.tvec.y=  translation[1];
+       CameraTransform_msg.tvec.z=  translation[2];
+       std::cout<<  "    translation: ";
+       for (int k=0 ; k<3 ; k++)
+       {
+         std::cout << translation[k] ;
+       }
+     }
+     cv::waitKey(10);
+     }
+   catch (cv_bridge::Exception& e)
+   {
+     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", image_->encoding.c_str());
+     ROS_ERROR("cv_bridge exception: %s", e.what());
+   }
   }
-
   void CameraTransformationPublisher(ros::Publisher* pub)
   {
     pub->publish(CameraTransform_msg);
-
   }
 };
-
-
-
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "extrinsicCalibration");
@@ -143,10 +120,7 @@ int main(int argc, char **argv)
   image_transport::ImageTransport it(nh);
   SubscribeAndPublish listener;
   ros::Publisher pub = nh.advertise<stereo_testbed::cameraTransformation>("CameraTransform", 1);
-
-
   image_transport::CameraSubscriber camerasub = it.subscribeCamera("/stereo/left/image_mono", 10, &SubscribeAndPublish::imageCallback, &listener);
-
   while(ros::ok())
   {
     listener.CameraTransformationPublisher(&pub);
